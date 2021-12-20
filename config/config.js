4900bd58a83576e3e85e94af2e57df7ac443095f
JJulessNL <73841047+JJulessNL@users.noreply.github.com>
2021-12-20 13:42:16 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,13 +8,21 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
   },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_TEST,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+  //test: {
+  //  username: process.env.DB_USER,
+  //  password: process.env.DB_PASSWORD,
+  //  database: process.env.DB_DATABASE_TEST,
+  //  host: process.env.DB_HOST,
+  //  port: process.env.DB_PORT,
+  //  dialect: process.env.DB_DIALECT,
+  //},
+    test: {
+    username: "root",
+    password: "isaac",
+    database: "isaac",
+    host:"localhost",
+    port: "3307",
+    dialect: "mysql",
   },
   "production": {
     "username": "root",
